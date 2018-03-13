23bfca9dc4b28e71d85e7f0244079e5b5fe8b955
aiswaryavg <aiswaryavgrupi@gmail.com>
2018-03-13 14:14:44 +0530

    submitted

---
+++ fact.py
@@ -0,0 +1,27 @@
+
+contentfile=open("number.py",'r')
+content = contentfile.read()
+a=input('enter the number')
+a=int(a)
+f=1
+for i in range(1,10):
+  f =((f*i))
+
+
+
+
+
+
+
+
+
+
+
+
+contentfile = open('number.py','r')
+content = contentfile.read()
+contetntfile.close()
+copyto.write('copy.py','w')
+copy.write(content)
+copy.close()
+
